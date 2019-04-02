804b388d6fcb3569ea654b706143a3a0055fedd6
ninja <srs@setdsp.ru>
2019-04-02 22:34:20 +0300

    Stop on client error

--- server/main.c
+++ server/main.c
@@ -27,18 +27,18 @@ int main()
 	struct sockaddr_storage clientaddr;
 	socklen_t socklen;
 	int listener;
-	char incoming_data[BUFFER_SIZE];
+	char incoming_data[MAX_BUFFER_SIZE];
 	int sock;
 	char remoteIP[INET_ADDRSTRLEN];
 	struct sockaddr_in addr;
 	int retv_recv;
 	ClientsOnline cOnline[MAX_ONLINE_CLIENTS];
 	sig_atomic_t srv_incomingSignal = 0;
-	
+
 	signal(SIGTERM, srv_incomingSignal_parse);
 	signal(SIGINT, srv_incomingSignal_parse);
 
-	
+
 	listener = socket(AF_INET, SOCK_STREAM, 0);
 	if(listener < 0)
 	{
@@ -99,7 +99,7 @@ int main()
 							FD_SET(new_socket, &main_set_fd);
 							if (new_socket > max_seen_socket)
 								max_seen_socket = new_socket;
-							
+
 							if (cIdx < 0)
 								cIdx++;
 							else
@@ -111,16 +111,16 @@ int main()
 								}
 								if (i == cIdx) //no empty slots
 									cIdx++;
-							} 
+							}
 							cOnline[cIdx].c_socket = new_socket;
 							cOnline[cIdx].inUse = 0;
 							cOnline[cIdx].c_addr = &clientaddr;
 							inet_ntop(clientaddr.ss_family,&(((struct sockaddr_in*)((struct sockaddr*)&clientaddr))->sin_addr),remoteIP, INET_ADDRSTRLEN );
-							
+
 							memcpy(&cOnline[cIdx].c_ip,&remoteIP,INET_ADDRSTRLEN);
 							i = 1;
 							setsockopt(cOnline[cIdx].c_socket, SOL_SOCKET, SO_REUSEADDR, &i, sizeof(int));
-							
+
 							i = fcntl(cOnline[cIdx].c_socket,F_GETFL,0);
 							if (i > 0)
 							{
@@ -133,7 +133,7 @@ int main()
 				}
 				else
 				{
-					if ((retv_recv = recv(current_socket, incoming_data, BUFFER_SIZE, 0)) <= 0)
+					if ((retv_recv = recv(current_socket, incoming_data, MAX_BUFFER_SIZE, 0)) <= 0)
 					{
 						if (retv_recv == 0)
 							printf("selectserver: ret socket %d", current_socket);
@@ -151,14 +151,16 @@ int main()
 						}
 						if (!cOnline[i].inUse)
 						{
-							cOnline[i].inUse = 1;
-								cOnline[i].c_targetSize = getFilesizeFromHeader(incoming_data);
-								cOnline[i].c_compressionType = getCompressionFromHeader(incoming_data);
-								printf("client %d[%d]: target: %d, compr:%d\n",
-										i, cOnline[i].c_socket, cOnline[i].c_targetSize, cOnline[i].c_compressionType);
+							if (getInfoFromHeader(incoming_data,retv_recv, &cOnline[i]))
+							{
+								cOnline[i].inUse = 1;
+								printf("client %d[%d]: data size: %d, compression:%d level:%d\n",
+									i, cOnline[i].c_socket, cOnline[i].c_targetSize, cOnline[i].c_compressionType,cOnline[i].c_compressionLevel);
+							}
 						}
 						processAndSend(current_socket, incoming_data, retv_recv, &cOnline[i]);
-						if (cOnline[i].c_rxSize == cOnline[i].c_targetSize)
+
+						if ((cOnline[i].c_rxSize == cOnline[i].c_targetSize) || (cOnline[i].c_error))
 						{
 							showCstat(&cOnline[i]);
 							FD_CLR(cOnline[i].c_socket, &main_set_fd);
